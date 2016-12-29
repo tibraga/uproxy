6efd9f9a9872020bf2a6f27e8cfd8b3668b754cd
Tiago Machado <tiago.machado@corp.globo.com>
2016-12-29 16:46:27 -0200

    Improving that produces problem

--- src/main/java/io/galeb/validate/uproxy/ReverseProxyServer.java
+++ src/main/java/io/galeb/validate/uproxy/ReverseProxyServer.java
@@ -20,11 +20,11 @@ package io.galeb.validate.uproxy;
 
 
 import io.undertow.Undertow;
+import io.undertow.server.HttpHandler;
 import io.undertow.server.handlers.ResponseCodeHandler;
-import io.undertow.server.handlers.proxy.ExclusivityChecker;
 import io.undertow.server.handlers.proxy.LoadBalancingProxyClient;
+import io.undertow.server.handlers.proxy.ProxyClient;
 import io.undertow.server.handlers.proxy.ProxyHandler;
-import io.undertow.util.Headers;
 import org.xnio.Options;
 
 import java.net.URI;
@@ -34,7 +34,7 @@ public class ReverseProxyServer {
 
     private static final String BACKEND_URI             = System.getProperty("client.backend", "http://127.0.0.1:80");
     private static final int    ROUTER_PORT             = Integer.parseInt(System.getProperty("server.port", "8000"));
-    private static final int    MAX_REQUEST_TIME        = Integer.parseInt(System.getProperty("server.maxRequestTime", "30000"));
+    private static final int    MAX_REQUEST_TIME        = Integer.parseInt(System.getProperty("client.maxRequestTime", "30000"));
     private static final int    CONNECTIONS_PER_THREAD  = Integer.parseInt(System.getProperty("client.connectionsPerThread", "20"));
     private static final int    IO_THREADS              = Integer.parseInt(System.getProperty("server.ioThread", "4"));
     private static final int    WORKER_THREADS          = Integer.parseInt(System.getProperty("server.workerThreads", Integer.toString(Runtime.getRuntime().availableProcessors()*8)));
@@ -42,42 +42,67 @@ public class ReverseProxyServer {
     private static final int    BACKLOG                 = Integer.parseInt(System.getProperty("server.backlog", "1000"));
     private static final boolean REWRITE_HOST_HEADER    = Boolean.valueOf(System.getProperty("server.rewriteHostHeader", Boolean.toString(false)));
     private static final boolean REUSE_X_FORWARDED      = Boolean.valueOf(System.getProperty("server.reuseXForwarded", Boolean.toString(true)));
+    private static final String LB_POLICY               = System.getProperty("server.loadBalancePolicy", "RoundRobin");
+
 
     public static void main(final String[] args) {
 
         if (args.length > 0 && "-h".equals(args[0])) {
             System.out.println(
-                "client.backend,              default: http://127.0.0.1:80\n" +
-                "server.port,                 default: 8000\n" +
-                "server.maxRequestTime,       default: 30000\n" +
-                "client.connectionsPerThread, default: 20\n" +
-                "server.ioThread,             default: 4\n" +
-                "server.workerThreads,        default: Runtime.getRuntime().availableProcessors()*8\n" +
-                "server.workerTaskMaxThreads, default: server.workerThreads\n" +
-                "server.backlog,              default: 1000\n" +
-                "server.rewriteHostHeader,    default: false\n" +
-                "server.reuseXForwarded,      default: true"
+                    "client.backend,              default: http://127.0.0.1:80\n" +
+                            "server.port,                 default: 8000\n" +
+                            "client.maxRequestTime,       default: 30000\n" +
+                            "client.connectionsPerThread, default: 20\n" +
+                            "server.ioThread,             default: 4\n" +
+                            "server.workerThreads,        default: Runtime.getRuntime().availableProcessors()*8\n" +
+                            "server.workerTaskMaxThreads, default: server.workerThreads\n" +
+                            "server.backlog,              default: 1000\n" +
+                            "server.rewriteHostHeader,    default: false\n" +
+                            "server.reuseXForwarded,      default: true\n" +
+                            "server.loadBalancePolicy,    default: RoundRobin\n" +
+                            "client.virtualhost,          default: local.domain.com"
             );
             System.exit(0);
         }
 
-        try {
-            final ExclusivityChecker exclusivityChecker = exchange -> {
-                // we always create a new connection for upgrade requests
-                return exchange.getRequestHeaders().contains(Headers.UPGRADE);
-            };
+        System.out.println(
+                "client.backend,              " + BACKEND_URI + "\n" +
+                 "server.port,                 " + ROUTER_PORT + "\n" +
+                 "client.maxRequestTime,       " + MAX_REQUEST_TIME + "\n" +
+                 "client.connectionsPerThread, " + CONNECTIONS_PER_THREAD + "\n" +
+                 "server.ioThread,             " + IO_THREADS + "\n" +
+                 "server.workerThreads,        " + WORKER_THREADS + "\n" +
+                 "server.workerTaskMaxThreads, " + WORKER_TASK_MAX_THREADS + "\n" +
+                 "server.backlog,              " + BACKLOG + "\n" +
+                 "server.rewriteHostHeader,    " + REWRITE_HOST_HEADER + "\n" +
+                 "server.reuseXForwarded,      " + REUSE_X_FORWARDED + "\n" +
+                 "server.loadBalancePolicy,    " + LB_POLICY + "\n"
+        );
 
-            LoadBalancingProxyClient loadBalancer = new LoadBalancingProxyClient(exclusivityChecker)
+
+        try {
+
+            //Init theproxy client
+            ProxyClient loadBalancer = new LoadBalancingProxyClient()
                     .addHost(new URI(BACKEND_URI))
+                    .setMaxQueueSize(0)
+                    .setSoftMaxConnectionsPerThread(5)
+                    .setProblemServerRetry(10)
+                    .setTtl(-1)
+                    .addSessionCookieName("JSESSIONID")
                     .setConnectionsPerThread(CONNECTIONS_PER_THREAD);
 
+            //Init the proxy handler default
+            HttpHandler proxyHandler = new ProxyHandler(loadBalancer, MAX_REQUEST_TIME, ResponseCodeHandler.HANDLE_500, REWRITE_HOST_HEADER, REUSE_X_FORWARDED);
+
+            //Init the Undertow
             Undertow reverseProxy = Undertow.builder()
                     .addHttpListener(ROUTER_PORT, "0.0.0.0")
                     .setIoThreads(IO_THREADS)
                     .setWorkerThreads(WORKER_THREADS)
                     .setWorkerOption(Options.WORKER_TASK_MAX_THREADS, WORKER_TASK_MAX_THREADS)
                     .setSocketOption(Options.BACKLOG, BACKLOG)
-                    .setHandler(new ProxyHandler(loadBalancer, MAX_REQUEST_TIME, ResponseCodeHandler.HANDLE_404, REWRITE_HOST_HEADER, REUSE_X_FORWARDED))
+                    .setHandler(proxyHandler)
                     .build();
 
             reverseProxy.start();
