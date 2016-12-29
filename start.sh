afeeae88a2888a4095de8b397e029a9e9bf58ffa
Tiago Machado <tiago.machado@corp.globo.com>
2016-12-29 16:47:03 -0200

    Added the script start

---
+++ start.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+java -Dclient.backend=http://127.0.0.1:8087 -Dserver.port=8011 -Dclient.maxRequestTime=0 -Dclient.connectionsPerThread=85 -Dserver.ioThread=4 -Dserver.workerThreads=$[4*24] -Dserver.backlog=65535 -Dserver.rewriteHostHeader=false -Dserver.reuseXForwarded=true -Dcom.sun.management.jmxremote.port=19998 -Dcom.sun.management.jmxremote.local.only=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -jar target/uproxy-1.0-SNAPSHOT-uber.jar
